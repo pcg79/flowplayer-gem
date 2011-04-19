cc2d7098d6fb82313d6965258746e353eb02fca4
Pat George <pat.george@gmail.com>
2011-04-19 16:02:05 -0400

    Add third parameter to flowplayer_for to match example and Player#initialize.

--- lib/flowplayer/railties/helper.rb
+++ lib/flowplayer/railties/helper.rb
@@ -1,16 +1,16 @@
 module Flowplayer
   module Helper
-    
+
     # flowplayer_for :hubble do |f|
     #  f.option 'foo'
     #  f.onLoad do
-    #    'this.unmute();'  
+    #    'this.unmute();'
     #  end
-    
-  
-    def flowplayer_for(id, swf, &block)
-      Player.new(id, swf, &block).script_tags.html_safe
+
+
+    def flowplayer_for(id, swf, lib='jquery', &block)
+      Player.new(id, swf, lib, &block).script_tags.html_safe
     end
-      
+
   end
 end
